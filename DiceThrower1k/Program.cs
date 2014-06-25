f20273da53ac2fd1495ebfe78f8001261e5d1eb2
Travis Whatley <travis.whatley121@gmail.com>
2014-06-25 14:14:03 -0600

    Dice Thrower 1k
    Finished and Commented

--- DiceThrower1k/Program.cs
+++ DiceThrower1k/Program.cs
@@ -10,6 +10,48 @@ namespace DiceThrower1k
     {
         static void Main(string[] args)
         {
+            //calling my ThrowDice function
+            ThrowDice("10d6");
+
+            ThrowDice("3d20");
+
+            ThrowDice("100d6");
+
+
+            //keep the console open
+            Console.ReadKey();
+        }
+        //creating the ThrowDice function
+        static void ThrowDice(string diceString)
+        {
+            //splitting diceString at the 'd'
+            string[] temp = diceString.Split('d');
+            //giving diceRolls the value of the first index after the split
+            string diceRolls = temp[0];
+            //giving diceSides the value of the next index after the split
+            string diceSides = temp[1];
+            //converting diceRolls to an int
+            int diceRollTot = int.Parse(diceRolls);
+            //converting diceSides to an int
+            int diceSideTot = int.Parse(diceSides);
+            //generating a random number
+            Random randomNumGen = new Random();
+            //for the number of rolls
+            //setting the random number
+            int randomNum = randomNumGen.Next(1, diceSideTot + 1);
+
+            //printing to the console
+            Console.WriteLine("Throwing: " + diceString);
+            Console.Write("Result #: " );
+           
+
+            for (int i = 1; i <= diceRollTot; i++)
+            {
+                Console.Write(randomNumGen.Next(1, diceSideTot + 1) + " ");
+            }
+
+            Console.WriteLine();
+            
         }
     }
 }
